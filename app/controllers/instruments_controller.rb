419e7b7a98be1b31632cb1f85e4efaa285f2ff06
JulianCedric <julian.pormentilla@gmail.com>
2020-06-11 13:01:19 -0400

    Update: ln.22 'READ.me' (artist --> instrument)

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -8,4 +8,16 @@ class InstrumentsController < ApplicationController
         @instrument = Instrument.find(params[:id]) 
     end 
     
+    def new 
+        @instrument = Instrument.new 
+        @artists = Artist.all  
+    end
+
+    def create 
+        @instrument = Instrument.create(instrument_params)
+        if @instrument.valid?
+            redirect_to artist_path(@artist)
+        else 
+
+
 end
